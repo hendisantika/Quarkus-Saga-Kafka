57bcacefde25ad1a669bec3e84a30cba61ad4ed9
hendisantika <hendisantika@yahoo.co.id>
2024-06-11 09:49:19 +0700

    Add OrderServiceApplication🫸🌀✏️📗🐧🐳

---
+++ order-service/src/main/java/id/my/hendisantika/OrderServiceApplication.java
@@ -0,0 +1,33 @@
+package id.my.hendisantika;
+
+import io.quarkus.runtime.Quarkus;
+import io.quarkus.runtime.QuarkusApplication;
+import io.quarkus.runtime.annotations.QuarkusMain;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : Quarkus-Saga-Kafka
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 6/11/24
+ * Time: 09:49
+ * To change this template use File | Settings | File Templates.
+ */
+@QuarkusMain
+@Slf4j
+public class OrderServiceApplication {
+
+    public static void main(String... args) {
+        Quarkus.run(MyApp.class, args);
+    }
+
+    public static class MyApp implements QuarkusApplication {
+        @Override
+        public int run(String... args) throws Exception {
+            Quarkus.waitForExit();
+            return 0;
+        }
+    }
+}
