32fdd5db449f9cde9f1fa3d948d7862b7c7785f0
hendisantika <hendisantika@yahoo.co.id>
2024-06-11 09:47:30 +0700

    Add receive in SeatEventConsumer🫸🌀✏️📗🐧🐳

--- order-service/src/main/java/id/my/hendisantika/event/SeatEventConsumer.java
+++ order-service/src/main/java/id/my/hendisantika/event/SeatEventConsumer.java
@@ -1,9 +1,14 @@
 package id.my.hendisantika.event;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import id.my.hendisantika.model.Seat;
 import id.my.hendisantika.service.SeatService;
+import io.smallrye.reactive.messaging.kafka.Record;
 import jakarta.enterprise.context.ApplicationScoped;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
+import org.eclipse.microprofile.reactive.messaging.Incoming;
 
 /**
  * Created by IntelliJ IDEA.
@@ -21,4 +26,18 @@ import lombok.extern.slf4j.Slf4j;
 public class SeatEventConsumer {
 
     private final SeatService seatService;
+
+    @SneakyThrows
+    @Incoming("seats-in")
+    public void receive(Record<Long, String> record) {
+        log.info("record es: {}", record.key());
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        var seat = objectMapper.readValue(record.value(), Seat.class);
+        if (null != seat.getType() && seat.getType().equalsIgnoreCase("compensation")) {
+            seatService.unlockSeat(seat.getId());
+        } else {
+            seatService.busySeat(seat.getId());
+        }
+    }
 }
