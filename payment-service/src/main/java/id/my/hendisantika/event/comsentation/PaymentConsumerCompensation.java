5b650a55f726229e82751340a6df841ed90501ca
hendisantika <hendisantika@yahoo.co.id>
2024-06-11 10:01:59 +0700

    Add receive in PaymentConsumerCompensation🫸🌀✏️📗🐧🐳

--- payment-service/src/main/java/id/my/hendisantika/event/comsentation/PaymentConsumerCompensation.java
+++ payment-service/src/main/java/id/my/hendisantika/event/comsentation/PaymentConsumerCompensation.java
@@ -1,8 +1,12 @@
 package id.my.hendisantika.event.comsentation;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import id.my.hendisantika.model.Payment;
 import jakarta.enterprise.context.ApplicationScoped;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
+import org.eclipse.microprofile.reactive.messaging.Incoming;
 
 /**
  * Created by IntelliJ IDEA.
@@ -21,5 +25,17 @@ public class PaymentConsumerCompensation {
 
     private final DeletePaymentUseCase deletePaymentUseCase;
 
-    private final org.lucasnscr.events.compensation.SeatEventProducer seatEventProducer;
+    private final SeatEventProducer seatEventProducer;
+
+    @SneakyThrows
+    @Incoming("allocate-in")
+    public void receive(Record<Long, String> record) {
+        log.info("Payment compensation with key {}", record.key());
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        var payment = objectMapper.readValue(record.value(), Payment.class);
+
+        deletePaymentUseCase.deletePayment(record.key());
+        seatEventProducer.sendSeatEvent(payment.getSeat());
+    }
 }
