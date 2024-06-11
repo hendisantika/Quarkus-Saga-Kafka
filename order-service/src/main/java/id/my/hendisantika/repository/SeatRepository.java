8d4b3636a4db936cd8f625756497d60160ee68bc
hendisantika <hendisantika@yahoo.co.id>
2024-06-11 09:41:36 +0700

    Add SeatRepository🫸🌀✏️📗🐧🐳

---
+++ order-service/src/main/java/id/my/hendisantika/repository/SeatRepository.java
@@ -0,0 +1,19 @@
+package id.my.hendisantika.repository;
+
+import id.my.hendisantika.model.Seat;
+import io.quarkus.hibernate.orm.panache.PanacheRepository;
+import jakarta.enterprise.context.ApplicationScoped;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : Quarkus-Saga-Kafka
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 6/11/24
+ * Time: 09:41
+ * To change this template use File | Settings | File Templates.
+ */
+@ApplicationScoped
+public class SeatRepository implements PanacheRepository<Seat> {
+}
