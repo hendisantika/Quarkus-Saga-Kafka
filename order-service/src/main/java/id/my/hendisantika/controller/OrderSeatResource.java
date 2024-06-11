41fe54ad3d04723ba4892138a40f7ac38915eb87
hendisantika <hendisantika@yahoo.co.id>
2024-06-11 09:50:20 +0700

    Add orderSeat in OrderSeatResource🫸🌀✏️📗🐧🐳

--- order-service/src/main/java/id/my/hendisantika/controller/OrderSeatResource.java
+++ order-service/src/main/java/id/my/hendisantika/controller/OrderSeatResource.java
@@ -7,8 +7,10 @@ import id.my.hendisantika.service.SeatService;
 import id.my.hendisantika.usecase.ReservedSeat;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
@@ -48,5 +50,13 @@ public class OrderSeatResource {
         log.info("New order received ");
         return Response.status(Response.Status.CREATED)
                 .entity(service.reservedSeat(seat)).build();
+    }
+
+    @GET
+    @Path("/{id}")
+    public Response orderSeat(@PathParam("id") Long id) {
+        log.info("Seat status by id");
+        return Response.status(Response.Status.OK)
+                .entity(seatService.findById(id)).build();
     }
 }
