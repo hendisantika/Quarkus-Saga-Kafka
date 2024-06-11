0a5368be6004f1fa71805a93152ff0cf7cd7ace2
hendisantika <hendisantika@yahoo.co.id>
2024-06-11 10:04:49 +0700

    Add PaymentService🫸🌀✏️📗🐧🐳

---
+++ payment-service/src/main/java/id/my/hendisantika/service/PaymentService.java
@@ -0,0 +1,30 @@
+package id.my.hendisantika.service;
+
+import id.my.hendisantika.event.comsentation.SeatEventProducer;
+import id.my.hendisantika.repository.PaymentRepository;
+import id.my.hendisantika.repository.SeatRepository;
+import jakarta.enterprise.context.ApplicationScoped;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : Quarkus-Saga-Kafka
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 6/11/24
+ * Time: 10:04
+ * To change this template use File | Settings | File Templates.
+ */
+@ApplicationScoped
+@RequiredArgsConstructor
+@Slf4j
+public class PaymentService {
+
+    private final PaymentRepository paymentRepository;
+
+    private final SeatEventProducer seatEventProducer;
+
+    private final SeatRepository seatRepository;
+}
