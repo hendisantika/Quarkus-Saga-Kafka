b4247a7f8600fb1ba25d114be844c68714151708
hendisantika <hendisantika@yahoo.co.id>
2024-06-11 10:05:41 +0700

    Add findAll in PaymentService🫸🌀✏️📗🐧🐳

--- payment-service/src/main/java/id/my/hendisantika/service/PaymentService.java
+++ payment-service/src/main/java/id/my/hendisantika/service/PaymentService.java
@@ -9,6 +9,9 @@ import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 /**
  * Created by IntelliJ IDEA.
  * Project : Quarkus-Saga-Kafka
@@ -43,5 +46,9 @@ public class PaymentService {
 
     public Payment findById(Long id) {
         return paymentRepository.findById(id);
+    }
+
+    public List<Payment> findAll() {
+        return paymentRepository.findAll().stream().collect(Collectors.toList());
     }
 }
