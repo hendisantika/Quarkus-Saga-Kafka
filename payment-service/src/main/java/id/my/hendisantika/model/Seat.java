9523d438bd71432c201efb5513707e67090b4882
hendisantika <hendisantika@yahoo.co.id>
2024-06-11 09:57:13 +0700

    Add Seat Model🫸🌀✏️📗🐧🐳

---
+++ payment-service/src/main/java/id/my/hendisantika/model/Seat.java
@@ -0,0 +1,51 @@
+package id.my.hendisantika.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Transient;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : Quarkus-Saga-Kafka
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 6/11/24
+ * Time: 09:56
+ * To change this template use File | Settings | File Templates.
+ */
+@Entity
+@Getter
+@Setter
+@ToString
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Seat {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Transient
+    private String type;
+
+    private Integer column;
+
+    private Integer row;
+
+    private String ref;
+
+    private String status;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    private User user;
+}
