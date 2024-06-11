392d214f0f58f9c98d68db63395869a0241b0946
hendisantika <hendisantika@yahoo.co.id>
2024-06-11 09:07:47 +0700

    Add load-data.sql🫸🌀✏️📗🐧🐳

---
+++ allocate-service/src/main/resources/load-data.sql
@@ -0,0 +1,12 @@
+INSERT INTO user(id, name, surname, email)
+VALUES (1, 'yuji', 'itadori', 'yuji@yopmail.com');
+INSERT INTO user(id, name, surname, email)
+VALUES (2, 'megumi', 'fushiguro', 'megumi@yopmail.com');
+INSERT INTO user(id, name, surname, email)
+VALUES (3, 'gojo', 'satoru', 'gojo@yopmail.com');
+INSERT INTO seat(id, column, row, status, user_id)
+VALUES (1, 1, 1, 'FREE', 1);
+INSERT INTO seat(id, column, row, status, user_id)
+VALUES (2, 1, 2, 'FREE', 1);
+INSERT INTO seat(id, column, row, status, user_id)
+VALUES (3, 1, 3, 'FREE', 1);
