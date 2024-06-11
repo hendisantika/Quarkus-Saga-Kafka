941eae36079b50bd5d24d57f3b1128d9fb077b1c
hendisantika <hendisantika@yahoo.co.id>
2024-06-11 09:18:49 +0700

    Add AllocateController🫸🌀✏️📗🐧🐳

---
+++ allocate-service/src/main/java/id/my/hendisantika/controller/AllocateController.java
@@ -0,0 +1,28 @@
+package id.my.hendisantika.controller;
+
+import id.my.hendisantika.service.SeatService;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : Quarkus-Saga-Kafka
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 6/11/24
+ * Time: 09:18
+ * To change this template use File | Settings | File Templates.
+ */
+@Path("/allocates")
+@RequiredArgsConstructor
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+@Slf4j
+public class AllocateController {
+    private final SeatService seatService;
+}
