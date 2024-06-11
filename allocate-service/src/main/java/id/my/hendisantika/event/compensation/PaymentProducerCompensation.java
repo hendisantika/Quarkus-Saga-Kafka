905336aa58b19ac99ae2e2662c19f9d430bf1f82
hendisantika <hendisantika@yahoo.co.id>
2024-06-11 09:14:01 +0700

    Add PaymentProducerCompensation🫸🌀✏️📗🐧🐳

---
+++ allocate-service/src/main/java/id/my/hendisantika/event/compensation/PaymentProducerCompensation.java
@@ -0,0 +1,45 @@
+package id.my.hendisantika.event.compensation;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import id.my.hendisantika.model.Payment;
+import jakarta.enterprise.context.ApplicationScoped;
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.eclipse.microprofile.reactive.messaging.Channel;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : Quarkus-Saga-Kafka
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 6/11/24
+ * Time: 09:13
+ * To change this template use File | Settings | File Templates.
+ */
+@ApplicationScoped
+@RequiredArgsConstructor
+@Slf4j
+public class PaymentProducerCompensation {
+
+    @Channel("payments-out")
+    Emitter<Record<Long, String>> emitter;
+
+    @SneakyThrows
+    public void sendPaymentEvent(Payment payment) {
+        log.info("Event sent {}", payment.getId());
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        var paymentJson = objectMapper.writeValueAsString(payment);
+
+        emitter.send(Record.of(payment.getId(), paymentJson))
+                .whenComplete((success, failure) -> {
+                    if (failure != null) {
+                        log.info("Message unprocessed failure " + failure.getMessage());
+                    } else {
+                        log.info("Message processed successfully");
+                    }
+                });
+    }
+}
